c9eb9e8b96749581d751322c3448895cadd33737
msnirmal <>
2023-12-03 22:41:05 +0000

    work on indentation error and tidy up

--- tasks/views.py
+++ tasks/views.py
@@ -7,17 +7,18 @@ from .forms import *
 # Create your views here.
 
 def index(request):
-    tasks = Task.objects.all()
+	tasks = Task.objects.all()
 
-    form = TaskForm()
+	form = TaskForm()
 
-    if request.method =='POST':
+	if request.method =='POST':
 		form = TaskForm(request.POST)
 		if form.is_valid():
 			form.save()
 		return redirect('/')
 
-    context = {'tasks':tasks, 'form':form}
+
+	context = {'tasks':tasks, 'form':form}
 	return render(request, 'tasks/list.html', context)
 
 def updateTask(request, pk):
