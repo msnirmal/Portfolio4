0aa814856f5bec4e29f7f1c9b8d718f074dadac7
msnirmal <>
2023-12-03 22:13:49 +0000

    create edit functionality to be able to make changes to the tasks added

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import *
@@ -19,3 +19,18 @@ def index(request):
 
     context = {'tasks':tasks, 'form':form}
 	return render(request, 'tasks/list.html', context)
+
+def updateTask(request, pk):
+	task = Task.objects.get(id=pk)
+
+	form = TaskForm(instance=task)
+
+	if request.method == 'POST':
+		form = TaskForm(request.POST, instance=task)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+
+	context = {'form':form}
+
+	return render(request, 'tasks/update_task.html', context)
