bca57a247461c661e948434a6edecb0c0cf39047
msnirmal <>
2023-12-06 19:54:52 +0000

    set up environment variables

--- todo/settings.py
+++ todo/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4!%f#qb^e7+#(s0qzaq+4w@w98z5*95zio@24ynz%y8tsi$6gh'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-4!%f#qb^e7+#(s0qzaq+4w@w98z5*95zio@24ynz%y8tsi$6gh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-msnirmal-portfolio4-q51zs7wwjb.us2.codeanyapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
