8064cdfcbc4aa7cc24a6f52716e5720c2a503772
msnirmal <>
2023-12-10 19:40:22 +0000

    set up environment variables & automatic deployment

--- todo/settings.py
+++ todo/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4!%f#qb^e7+#(s0qzaq+4w@w98z5*95zio@24ynz%y8tsi$6gh'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-4!%f#qb^e7+#(s0qzaq+4w@w98z5*95zio@24ynz%y8tsi$6gh')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['todo-2023-app-8c29f27afc8c.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
